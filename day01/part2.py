ced612702e9edde69412052629c1bfc3d8903af3
bugra-yilmaz <bugra.yyilmaz@gmail.com>
2024-12-01 07:20:14 +0100

    solve day01

---
+++ day01/part2.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    left = []
+    right = []
+    for line in s.splitlines():
+        number_left, number_right = line.split()
+        left.append(int(number_left))
+        right.append(int(number_right))
+
+    result = 0
+    for number_left in left:
+        result += number_left * right.count(number_left)
+
+    return result
+
+
+INPUT_S = '''\
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+'''
+EXPECTED = 31
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
